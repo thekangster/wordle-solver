269992dc21120657d3d59b2c34827b8277a975fc
Kevin Kang <ktkang@ucsc.edu>
2022-01-26 13:10:59 -0800

    cloned everything

---
+++ hw2/num_arrays.c
@@ -0,0 +1,28 @@
+
+/*
+ * All of your code goes in here. You need to finish these functions.
+ */
+
+int maximum(int *nums, int len) {
+  return 0;
+}
+
+int sum_positive(int *nums, int len) {
+  return 0;
+}
+
+int reduce(int *nums, int len, int (*f)(int,int), int initial){
+  return 0;
+}
+
+int maximum_with_reduce(int *nums, int size) {
+  return 0;
+}
+
+int sum_positive_with_reduce(int *nums, int size) {
+  return 0;
+}
+
+int count_negative_with_reduce(int *nums, int size) {
+  return 0;
+}
