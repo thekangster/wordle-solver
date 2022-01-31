fa3cad56521578941389aa9c2c4ddae8ecabb1ae
Kevin Kang <ktkang@ucsc.edu>
2022-01-31 14:06:18 -0800

    initial clone

---
+++ hw3/stack_calculator.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "stack_calculator.h"
+
+// Allocate a new Stack struct and return a pointer to it.
+Stack *stack_create(void) {
+  // this one is complete, no need to change it!
+  Stack *out;
+  out = (Stack *)calloc(1, sizeof(Stack));
+  return out;
+}
+
+// Add a new node at the front of the stack. Return false if we are unable to do
+// so (eg, if memory allocation fails).
+bool stack_push(Stack *s, CalculatorItem item) {
+  // your code here
+  return true;
+}
+
+// Pop the stack and put the result into the output pointer.
+// return false if the stack is NULL or if it is empty.
+bool stack_pop(Stack *s, CalculatorItem *output) {
+  // your code here
+  return true;
+}
+
+// Returns true if the specified stack is empty.
+bool stack_empty(Stack *s) {
+  // your code here
+  return true;
+}
+
+// Free all of the memory used by this stack, including its linked list.
+// When you are done, set the pointer to NULL.
+void stack_delete(Stack **s) {
+  // your code here
+}
+
+// Returns true if we were able to successfully perform the computation step,
+// and false if there was an error of some kind.
+bool stack_compute_step(Stack *s, CalculatorItem item) {
+  // your code here
+  return true;
+}
