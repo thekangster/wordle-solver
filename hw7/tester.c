48e14917593f45e081e9db19f4d04956e2956b17
Kevin Kang <ktkang@ucsc.edu>
2022-03-08 23:28:31 +0000

    not reading the first line

--- hw7/tester.c
+++ hw7/tester.c
@@ -9,17 +9,34 @@
 
 int main(void) {
 
-  CustomerNode *newcust
-  FILE *infile = fopen("customers.tsv", "r");
-  while (fscanf(infile, " %50[^\t]\t%50[^\t]\t%d\t%50[^\n]",  
-
-
-
-
-  
   CustomerNode* buckets[NUM_BUCKETS] = {NULL};
+ 
+  CustomerNode *newcust;
+
+  newcust = malloc(sizeof(CustomerNode));
+  newcust->email = (char *)malloc(50*sizeof(char *));
+  newcust->name = (char *)malloc(50*sizeof(char *));
+  newcust->fav_food = (char *)malloc(50*sizeof(char *));
+
+  char buf[150];
+
+  FILE *infile = fopen("customers.tsv", "r");
+  if (infile == NULL) {
+    printf("no such file\n");
+  }
+
+  while (fgets(buf, 150, infile)) {
+    if (fscanf(infile, "%50[^\t]\t%50[^\t]\t%d\t%50[^\n]", newcust->email, newcust->name, &newcust->shoe_size, newcust->fav_food) == 4) {
+      add_to_hashtable(newcust->email, newcust->name, newcust->shoe_size, newcust->fav_food, buckets, NUM_BUCKETS);
+      printf("email: %s\tname: %s\tshoe size: %d\tfav food: %s\n", newcust->email, newcust->name, newcust->shoe_size, newcust->fav_food);
+    } else {
+      printf("missing email, name, shoe_size, or fav_food\n"); 
+    }
+  } 
+
+/* 
   int sammy_size = 20;
-  
+
   add_to_hashtable("sammy@ucsc.edu", "sammy", sammy_size, "slugs", buckets, NUM_BUCKETS);
   add_to_hashtable("jim@ucsc.edu", "jimothy", 12, "borgar", buckets, NUM_BUCKETS);
 
@@ -27,7 +44,7 @@ int main(void) {
   char *jim_food = fav_food_for_customer("jim@ucsc.edu", buckets, NUM_BUCKETS);
   printf("fav food for sammy@ucsc.edu: %s\n", sammy_food);
   printf("fav food for jim@ucsc.edu: %s\n", jim_food);
-
+*/
   return 0;
 }
 
