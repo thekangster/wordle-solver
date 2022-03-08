3c97cb51ed83bd25fd080675f8fe077a35f646c9
Kevin Kang <ktkang@ucsc.edu>
2022-03-08 00:27:50 -0800

    wrote tests

--- hw7/hw7.c
+++ hw7/hw7.c
@@ -35,6 +35,23 @@ CustomerNode *add_to_list(char *email, CustomerInfo data, CustomerNode *bucket) 
   return new_customer;
 }
 
+char *fav_food_for_customer(char *email, CustomerNode **buckets, size_t num_buckets) {
+  
+  size_t which_bucket = hash(email) % num_buckets;
+
+  char *result = NULL;
+  CustomerNode* node;
+  node = buckets[which_bucket];
+
+  while(node != NULL) {
+    if (strcmp(node->key, email) == 0) {
+      result = node->val->fav_food;
+    }
+    node = node->next;
+  }
+  return result;
+}
+
 void add_to_hashtable(char *email, CustomerInfo data, CustomerNode **buckets, size_t num_buckets) {
   
   size_t which_bucket = hash(email) % num_buckets;
@@ -45,5 +62,21 @@ void add_to_hashtable(char *email, CustomerInfo data, CustomerNode **buckets, si
 }
 
 int main(void) {
+  CustomerNode* buckets[NUM_BUCKETS] = {NULL};
+  CustomerInfo* sammy;
+  sammy = malloc(sizeof(CustomerInfo));
+  sammy->name = "sam the slug";
+  sammy->email = "sammy@ucsc.edu";
+  sammy->shoe_size = 20;
+  sammy->fav_food = "slugs";
+  
+  printf("%s\n%s\n%d\n%s\n", sammy->name, sammy->email, sammy->shoe_size, sammy->fav_food);
+  add_to_hashtable("sammy@ucsc.edu", *sammy, buckets, NUM_BUCKETS);
+  printf("made it here\n"); 
+
+  char *fav_food = NULL;
+  fav_food = fav_food_for_customer("sammy@ucsc.edu", buckets, NUM_BUCKETS);
+  printf("fav food for sammy@ucsc.edu: %s\n", fav_food);
+
   return 0;
 }
